56600884af0f397b52076b20ff06a2aa276429c2
José Vergara <josea.vergara2005@gmail.com>
2025-03-25 00:32:20 -0600

    ADDED: heap_sort

--- practica-1/lib/sorting/heap.c
+++ practica-1/lib/sorting/heap.c
@@ -1,4 +1,39 @@
+void heapify(int *array, unsigned int size, int index);
+
 void heap_sort(int *array, unsigned int size)
 {
+	for (int i = (size / 2) - 1; i >= 0; i--)
+		heapify(array, size, i);
+
+	for (int i = size - 1; i > 0; i--) {
+		int temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+
+		heapify(array, i, 0);
+	}
 	return;
+}
+
+void heapify(int *array, unsigned int size, int index)
+{
+	int largest = index;
+
+	int left = 2 * index + 1;
+
+	int right = 2 * index + 2;
+
+	if (left < size && array[left] > array[largest])
+		largest = left;
+	if (right < size && array[right] > array[largest])
+		largest = right;
+
+	if (largest != index) {
+		int temp = array[index];
+		array[index] = array[largest];
+		array[largest] = temp;
+
+		heapify(array, size, largest);
+	}
+
 }
