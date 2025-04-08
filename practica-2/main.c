4f3e1a0782b33c1f73b5d22cf68c75744d271b2d
José Vergara <josea.vergara2005@gmail.com>
2025-04-08 00:07:02 -0600

    DOC: fucntions in main

--- practica-2/main.c
+++ practica-2/main.c
@@ -1,3 +1,29 @@
+/**
+ * @file main.c
+ * 
+ * @author Bautista Quijano Anayeli
+ * @author Garcia Morlan Julio Amauri
+ * @author Jimenez Rodriguez Alan Gael Gael
+ * @author Hernandez Zamora Valeria
+ * @author Vergara Gamboa José Alfonso
+ * 
+ * @date Apr-2025
+ * 
+ * @version 1.0.0
+ * 
+ * Descripción:
+ * El programa tiene como función comparar diversos algoritmos de busqueda,
+ * tanto single-threaded como multi-threaded,
+ * por lo que mide el tiempo que tardan en procesar y lo devuelve al usuario.
+ * 
+ * El número de hilos a usar en la implementación multithreaded de cada algoritmo
+ * esta hardcodiado, pero puede ser facilmente remplazable por un argumento.
+ * 
+ * Compilación:
+ * 	make
+ * Ejecución ./sort.out
+ * 
+ */
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,42 +32,106 @@
 #include "lib/search.h"
 #include "lib/tiempo.h"
 
+/**
+ * @enum SearchingAlgorithms
+ * @brief Enumera varios tipos de algoritmos de busqueda, incluyendo sus versiones estandard y multi-threaded
+ * 
+ * Este enum define un conjunto de algoritmos de busqueda que pueden ser usados para buscar elementos
+ * dentro de un arreglo. Las variantes con el sufijo '_T' indican versiones multi-threaded, las cuales
+ * utilizan multiples hilos para distrubuir la workload
+ */
 enum SearchingAlgorithms {LINEAR, BINARY, BINARY_TREE, EXPONENTIAL, FIBONACCI, LINEAR_T, BINARY_T, BINARY_TREE_T, EXPONENTIAL_T, FIBONACCI_T};
 
+/**
+ * @typedef searching_function
+ * @brief Apuntador de funcion para algoritmo de ordenamiento.
+ * 
+ * Esta typedef define un apuntador para algoritmos de ordenamiento que operan
+ * en arreglos de enteros.
+ * 
+ * @param array Apuntador al arreglo de enteros en el que buscar.
+ * @param size El numero de elementos en el arreglo
+ * @param target El valor del entero a buscar en el arreglo
+ * 
+ * @return El index del numero target en el arreglo si se encontro, -1 en caso contrario
+ */
 typedef int (*searching_function) (int *array, unsigned int size, int target);
 
+/**
+ * @brief Imprime las instrucciones de uso para el programa.
+ * 
+ * Esta funcion muestra una guia para usar el programa desde la terminal,
+ * incluyendo los argumentos necesarios y una lista de los algoritmos disponibles,
+ * con sus codigos correspondientes.
+ * 
+ * @param programName El nombre del archivo ejecutable, normalemente pasado al programa como 'argv[0]' desde 'main'
+ */
 void printUsage(char *programName);
 
+/**
+ * @brief Recupera el valor correspondiente del enum de algoritmos de busqueda de acuerdo a 'option'
+ * 
+ * La funcion mapea una cadena que representa a un algoritmo de busqueda. La funcion utiliza el bool 'isUsingThreads'
+ * para determinar si debe regresar la version estandard o multi-threaded.
+ * 
+ * @param option Una cadena representando un algoritmo de busqueda
+ * @param isUsingThreads Bandera booleana que indica si el algoritmo es multi-threaded
+ * 
+ * @return El correspondiente valor del enum de algoritmos, o '-1' si la opciob es invalida
+ */
 int getSearchingAlgorithm(char *option, bool isUsingThreads);
 
+/**
+ * @brief Selecciona un algoritmo de busqueda de acuerda a la opcion dada.
+ * 
+ * La funcion toma una de las opciones del enum 'searchingAlgorihtms' y
+ * retorna el apuntador a la funcion de busqueda correspondiente.
+ * 
+ * @param option El algoritmo de busqueda seleccionado.
+ * 
+ * @return Un apuntador a la funcion de busqueda basada en 'option', o 'NULL' si no se proporciona una opcion valida.
+ * 
+ * @note El apuntador que regresa esta funcion puede ser usado para invocar el algoritmo de busqueda correspondiente.
+ */
 searching_function selectSearchingAlgorithm(enum SearchingAlgorithms option);
 
 int main(int argc, char *argv[])
 {
-	enum SearchingAlgorithms selectedAlgorithm;
-	int target;
-	unsigned int size;
+	// Declaracion de variables
+	enum SearchingAlgorithms selectedAlgorithm; ///< Guarda el algoritmo seleccionado
+	unsigned int size; ///< Tamaño del arreglo
+	int target; ///< El valor a buscar
 	
+	// Checa si el numero de argumentos es correcto
 	if (argc != 6) {
 		printUsage(argv[0]);
 		exit(1);
 	} else {
-		target = atoi(argv[1]);
+		// Obtiene el valor a buscar
+		target = atoi(argv[1]); ///< Convierte el primer argumento a un entero
+
+		// Probamos si el segundo argumento es el esperado '-s'
 		if (strcmp(argv[2], "-s") != 0) {
 			printf("ERROR: Unknown argument %s.\n\n", argv[2]);
 			printUsage(argv[0]);
 			exit(1);
 		}
-		size = atoi(argv[3]);
+
+		// Obtenemos el tamaño del argumento
+		size = atoi(argv[3]); ///< Convierte el tercer argumento a un entero
+
+		// Checa el argumento de seleccion de algoritmo
 		if (strcmp(argv[4], "-at") == 0) {
-			selectedAlgorithm = getSearchingAlgorithm(argv[5], true);
+			selectedAlgorithm = getSearchingAlgorithm(argv[5], true); //< Selecciona algortmos threaded
 		} else if (strcmp(argv[4], "-a") == 0) {
-			selectedAlgorithm = getSearchingAlgorithm(argv[5], false);
+			selectedAlgorithm = getSearchingAlgorithm(argv[5], false); //< Selecciona algoritmos estandar
 		} else {
 			printf("ERROR: Unknown argument %s.\n\n", argv[4]);
 			printUsage(argv[0]);
 			exit(1);
 		}
+
+		// Valida el algoritmo seleccionado
 		if (selectedAlgorithm == -1) {
 			printf("ERROR: Unknown algorithm option %s.\n\n", argv[5]);
 			printUsage(argv[0]);
@@ -49,7 +139,10 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	// Selecciona la funcion de busqueda basada en la opcion seleccionada
 	searching_function search = selectSearchingAlgorithm(selectedAlgorithm);
+
+	// Alojamos memoria para el arreglo
 	int *array = malloc(size * sizeof(*array));
 	for (int i = 0; i < size; i++)
 		scanf("%d", &array[i]);
@@ -113,14 +206,13 @@ int getSearchingAlgorithm(char *option, bool isUsingThreads)
 		if (strcmp(option, "E") == 0) return EXPONENTIAL_T;
 		if (strcmp(option, "F") == 0) return FIBONACCI_T;
 		return -1;
-	} else {
-		if (strcmp(option, "L") == 0) return LINEAR;
-		if (strcmp(option, "B") == 0) return BINARY;
-		if (strcmp(option, "T") == 0) return BINARY_TREE;
-		if (strcmp(option, "E") == 0) return EXPONENTIAL;
-		if (strcmp(option, "F") == 0) return FIBONACCI;
-		return -1;
 	}
+	if (strcmp(option, "L") == 0) return LINEAR;
+	if (strcmp(option, "B") == 0) return BINARY;
+	if (strcmp(option, "T") == 0) return BINARY_TREE;
+	if (strcmp(option, "E") == 0) return EXPONENTIAL;
+	if (strcmp(option, "F") == 0) return FIBONACCI;
+	return -1;
 }
 
 searching_function selectSearchingAlgorithm(enum SearchingAlgorithms option)
