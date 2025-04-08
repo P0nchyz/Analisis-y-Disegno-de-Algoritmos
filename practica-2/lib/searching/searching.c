8427cfe07abf394e226e53984679d2ef3e69e870
ValHze <valhzamora@gmail.com>
2025-04-07 22:24:01 -0600

    Update searching.c
    lolo

--- practica-2/lib/searching/searching.c
+++ practica-2/lib/searching/searching.c
@@ -51,5 +51,39 @@ int exponential_search(int *array, unsigned int size, int target)
 
 int fibonacci_search(int *array, unsigned int size, int target)
 {
+	int fib_prev2 = 0;                      // (m-2)º número de Fibonacci
+	int fib_prev1 = 1;                      // (m-1)º número de Fibonacci
+	int fib_current = fib_prev1 + fib_prev2; // m-ésimo número de Fibonacci
 
+	// Encuentra el menor número de Fibonacci >= size
+	while (fib_current < size) {
+		fib_prev2 = fib_prev1;
+		fib_prev1 = fib_current;
+		fib_current = fib_prev1 + fib_prev2;
+	}
+
+	int search_start = -1;
+
+	while (fib_current > 1) {
+		int i = (search_start + fib_prev2 < size - 1) ? (search_start + fib_prev2) : (size - 1);
+
+		if (array[i] < target) {
+			fib_current = fib_prev1;
+			fib_prev1 = fib_prev2;
+			fib_prev2 = fib_current - fib_prev1;
+			search_start = i;
+		} else if (array[i] > target) {
+			fib_current = fib_prev2;
+			fib_prev1 = fib_prev1 - fib_prev2;
+			fib_prev2 = fib_current - fib_prev1;
+		} else {
+			return i;
+		}
+	}
+
+	// Comparar el último elemento restante
+	if (fib_prev1 && search_start + 1 < size && array[search_start + 1] == target)
+		return search_start + 1;
+
+	return -1;
 }
